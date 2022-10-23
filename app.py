be8500ac69ba9b97049d6f0ccc55f425f6ec422e
Harshal <113486674+angryprincess@users.noreply.github.com>
2022-10-23 19:46:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app.debug = True
 @app.route('/webhook', methods=['POST'])
 def index():
     body = request.json
+    city = body['queryResult']['parameters']['geo-city']
     
-    api_url = 'https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=19bc09406ba467d4ea980ffe2deca519'
+    api_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=19bc09406ba467d4ea980ffe2deca519'
     headers = {'Content-Type': 'application/json'}
     response = requests.get(api_url, headers=headers)
     r=response.json()
 
-    city = body['queryResult']['parameters']['geo-city']
     temp = str(int(r['main']['temp']))
     sunrise = str(r["sys"]["sunrise"])
     ts = int(sunrise)
